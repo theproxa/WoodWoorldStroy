f22c717a4a340cd33d317297181d378a116dcbf7
arsmik <95arsmik95@gmail.com>
2025-02-26 03:46:27 +0300

    redy to deploy

--- WoodWoorldStroy/WoodWoorldStroy/settings.py
+++ WoodWoorldStroy/WoodWoorldStroy/settings.py
@@ -126,6 +126,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 STATICFILES_DIRS = [
     BASE_DIR / "templates/src",

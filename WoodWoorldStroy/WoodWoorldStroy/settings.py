b33a75d1cfa93e874ec146040462c51c7bc3f133
arsmik <95arsmik95@gmail.com>
2025-04-12 22:28:03 +0300

    set nginx

--- WoodWoorldStroy/WoodWoorldStroy/settings.py
+++ WoodWoorldStroy/WoodWoorldStroy/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', '10.13.100.205']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', '10.13.100.205', 'woodworldstroy.ru']
 
 
 # Application definition
